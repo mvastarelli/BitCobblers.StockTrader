8145e4ebea0a2d2dafbe836b87dfe239ffe27d74
Vastarelli, Michael <michael.vastarelli@changehealthcare.com>
2019-11-23 20:01:20 -0500

    Add project files.

---
+++ BitCobblers.StockTrader/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BitCobblers.StockTrader.Models;
+
+namespace BitCobblers.StockTrader.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
